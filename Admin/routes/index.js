4b10c99fe556087c6e811c257c32c4a3a1275084
kobowon <ts3507@ajou.ac.kr>
2018-05-03 00:56:02 +0900

    2018-05-03-고보원-간략한 getVolunteerList API

--- Admin/routes/index.js
+++ Admin/routes/index.js
@@ -5,6 +5,50 @@ var connection = mysql_dbc.init();
 var path = require('path');
 mysql_dbc.test_open(connection);
 console.log('Hello world')
+//시간, 종류, 장애인id, 장애인 위치
+/*
+  type varchar(30) NOT NULL,
+  helpeeID varchar(30) NOT NULL,
+  position varchar(100) NOT NULL,
+ */
+
+router.get('/helper/getVolunteerList',function (req,res) {
+    var stmt = 'select * from volunteerItem';
+    connection.query(stmt,function(err,result){
+        if(err) throw err;
+        res.send(JSON.stringify(result));
+    })
+})
+
+router.post('/helpee/requestVolunteer',function(req,res){
+    var body = req.body;
+    var type = body.type;
+    var helpeeID = body.helpeeID;
+    var position = body.position;
+    connection.query('insert into volunteerItem (type,helpeeID,position) values ("' + type + '","' + helpeeID + '","' + position + '")',function (err,result) {
+        if(err) { throw err;}
+        console.log("Data inserted!");
+        res.send(JSON.stringify(result));
+    })
+})
+
+router.get('/join', function(req,res){
+    res.sendFile(path.join(__dirname, '../public/join.html'))
+})
+
+router.post('/join', function(req,res){
+    console.log('req.body is',req.body);
+    var body = req.body;
+    var email = body.email;
+    var name = body.name;
+    var passwd = body.password;
+
+    var query = connection.query('insert into user (email, name, password) values ("' + email + '","' + name + '","' + passwd + '")', function(err, rows) {
+        if(err) { throw err;}
+        console.log("Data inserted!");
+        res.send("Data inserted!");
+    })
+})
 
 router.get('/test', function (req, res) {
 
@@ -27,23 +71,6 @@ router.get('/test', function (req, res) {
        });
     })
 });
-
-router.get('/join', function(req,res){
-    res.sendFile(path.join(__dirname, '../public/join.html'))
-})
-
-router.post('/', function(req,res){
-    var body = req.body;
-    var email = body.email;
-    var name = body.name;
-    var passwd = body.password;
-
-    var query = connection.query('insert into user (email, name, password) values ("' + email + '","' + name + '","' + passwd + '")', function(err, rows) {
-        if(err) { throw err;}
-        console.log("Data inserted!");
-    })
-})
-
 
 //helper login
 router.get('/helper/login', function (req, res) {
