fa223195c8f7e6b894dbccf0431797f02d688179
kobowon <ts3507@ajou.ac.kr>
2018-05-07 23:54:52 +0900

    2018-05-07-고보원-user 삭제 API & 봉사 ID로 봉사리스트가져오기 API

--- Admin/routes/admin.js
+++ Admin/routes/admin.js
@@ -16,7 +16,7 @@ router.get('/getUserList/:initID',function (req,res) {
 })
 
 //userID 로 봉사리스트 가져오기
-router.get('/getVolunteerList/:userID',function (req,res) {
+router.get('/getVolunteerListByUserID/:userID',function (req,res) {
     console.log(req.params.userID);
     var stmt = 'select * from volunteerItem where helper_ID = ? OR helpee_ID = ?';
     var params = [req.params.userID,req.params.userID];
@@ -24,6 +24,27 @@ router.get('/getVolunteerList/:userID',function (req,res) {
         if(err) throw err;
         res.send(JSON.stringify(result));
     })
-})
+});
+
+//volunteer_id 로 봉사리스트 가져오기
+router.get('/getVolunteerListByVolunteerID/:volunteer_id',function (req,res) {
+    console.log(req.params.volunteer_id);
+    var stmt = 'select * from volunteerItem where volunteer_id = ?';
+    connection.query(stmt,req.params.volunteer_id,function(err,result){
+        if(err) throw err;
+        res.send(JSON.stringify(result));
+    })
+});
+
+//userID로 유저에서 삭제
+//{"userID": "tt"} 이런 형태로 post 처럼 보내면 됨
+router.delete('/removeUser',function(req,res){
+    var stmt = 'DELETE FROM user WHERE userID = ?';
+    connection.query(stmt,req.body.userID,function(err,result){
+        if(err) throw err;
+        res.send(JSON.stringify(result));
+    })
+});
+
 module.exports = router;
 
