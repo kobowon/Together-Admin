8a312d32305a9068fba25204601f53395733c015
JSY <soyoung6488@ajou.ac.kr>
2018-05-11 00:06:55 +0900

    2018-05-11-정소영-db로 부터 userID 받아와 리스트 만들기 초기 작업

--- Admin/public/template/js/custom.js
+++ Admin/public/template/js/custom.js
@@ -20,20 +20,48 @@
 				error:function () {
 					alert(url+text);
                 },
-				success: function (data) {
-					alert(data);
+				success: function (userData) {
+				    var i=0,length=Object.keys(userData).length;;
+				    sort_down_by_name(userData);
+				    for(i; i<=length-1;i++)
+                    {
+                        var $list_div= $('<div class="listing-item mb-20">' +
+                            '<div class="row grid-space-0">' +
+                            '<div class="col-md-6 col-lg-4 col-xl-2">' +
+                            '<div class="overlay-container id=img_container'+i+'"></div></div>' +
+                            '<div class="col-md-6 col-lg-8 col-xl-9">' +
+                            '<div class="body" id = "list_body'+i+'"></div>' +
+                            '</div></div></div>');
+                        $('#down_list').append($list_div);
+                        /*$('img_container'+i).appendChild(<img src="")//profile_image blob으로 읽힘*/
+                        var $list_header = $('<h3 class="margin-clear">'+userData[i].userID+'</h3>');
+                        var list_body_id='#list_body'+i;
+                        $(list_body_id).append($list_header);
+                        /*$('up_list').appendChild($list_div);*/
+                    }
                 }
 			});
         });
 
-        $("#pill-1").click(function(){
 
-        });
-        $("#pill-2").click(function(){
-
-        });
 	}); // End document ready
 
 })(jQuery);
 
+//userID 오름차순으로 정렬
+function sort_up_by_name_(userObjects){
+    userObjects.sort(function(a,b){
+        return a.userID < b.userID ? -1 : a.userID > b.userID ? 1 : 0;
+    })
+}
 
+//userID 내림차순으로 정렬
+function sort_down_by_name(userObjects){
+    userObjects.sort(function(a,b){
+        return a.userID > b.userID ? -1 : a.userID < b.userID ? 1 : 0;
+    })
+}
+
+function makeUserList(htmlID,userID,userType,score){
+
+}
