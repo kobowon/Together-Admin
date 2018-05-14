7f9d55b7445e191bcbfb0ec19e21f15aa38f0b9b
JSY <soyoung6488@ajou.ac.kr>
2018-05-14 20:41:41 +0900

    2018-05-14-정소영-사용자 상세보기 커스텀 모달 title 동적으로 넣기 별점 추가

--- Admin/public/template/js/custom.js
+++ Admin/public/template/js/custom.js
@@ -10,6 +10,8 @@
 	"use strict";
 
 	$(document).ready(function() {
+
+	    //조회버튼 동작
 	    $('#search_text').change(function () {
 	        $('.listing-item').remove();
             $("#search_btn").one('click',function () {
@@ -40,15 +42,7 @@
                             var $list_header = $('<h3 class="margin-clear">'+userData[i].userID+'</h3>');
                             var list_body_id='#list_body'+i;
                             $(list_body_id).append($list_header);
-                            var score='';
-                            var j;
-                            for(j=1;j<=userData[i].userFeedbackScore;j++){
-                                score = score + '<i class="fa fa-star text-default"></i>';
-                            }
-                            for(j=1;j<=5-userData[i].userFeedbackScore;j++){
-                                score= score + '<i class="fa fa-star"></i>'
-                            }
-                            var $in_body = $('<p>'+score+'<a href="#" class="btn-sm-link"><i class="fa fa-search" data-toggle="modal" data-target="#user_detail" data-userid='+userData[i].userID+'>상세보기</i></a></p>'+'<div class="elements-list clearfix">' +
+                            var $in_body = $('<p>'+score_star(userData[i].userFeedbackScore)+'<a href="#" class="btn-sm-link"><i class="fa fa-search" data-toggle="modal" data-target="#user_detail" data-userid='+userData[i].userID+' data-usertype='+userData[i].userType+' data-score='+userData[i].userFeedbackScore+'>상세보기</i></a></p>'+'<div class="elements-list clearfix">' +
                                 '<a href="#" class="pull-right btn btn-sm btn-animated btn-danger btn-default-transparent">탈퇴시키기<i class="fa fa-times"></i></a>' +
                                 '</div>');
                             $(list_body_id).append($in_body);
@@ -58,21 +52,29 @@
                 });
             });
         })
-	}); // End document ready
 
-    $('#user_detail').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var recipient = button.data('userid'); // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
-        $('#user_detail_modal_header').empty();
-        $('#user_detail_modal_header').append('<h4 class="modal-title" id="user_detailModalLabel">'+recipient+'</h4>' +
-            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>');
-        $('#user_detail_modal_body').empty();
-        $('#user_detail_modal_body').append('<div class="col-lg-auto" id="user_detailModalContent">내용</div>');
-    });
-
+        //사용자 상세보기 모달 내용 동적으로 넣기
+        $('#user_detail').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget); // Button that triggered the modal
+            var userID = button.data('userid'); // Extract info from data-* attributes
+            var userType = button.data('usertype');
+            var userScore =button.data('score');
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var modal = $(this);
+            $('#user_detail_modal_header').empty();
+            $('#user_detail_modal_header').append('<h4 class="modal-title" id="user_detailModalLabel">'+userID+'</h4>' +
+                '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>');
+            var score = '<p>'+score_star(userScore)+'</p>';
+            var $modal_body = $(
+                '<div class="col-lg-auto" id="user_detailModalContent">' +
+                    '<p>사용자 타입 : '+userType+'</p>'+
+                    score +
+                '</div>');
+            $('#user_detail_modal_body').empty();
+            $('#user_detail_modal_body').append($modal_body);
+        });
+    }); // End document ready
 })(jQuery);
 
 //userID 오름차순으로 정렬
@@ -125,4 +127,16 @@ function makeUserList(userData){
 
 function show_userDetailModal(userData){
 
+}
+
+function score_star(score){
+    var sccore_star='';
+    var j;
+    for(j=1;j<=score;j++){
+        sccore_star = sccore_star + '<i class="fa fa-star text-default"></i>';
+    }
+    for(j=1;j<=5-score;j++){
+        sccore_star= sccore_star + '<i class="fa fa-star"></i>'
+    }
+    return sccore_star;
 }
