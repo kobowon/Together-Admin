f59e54935a35121349af369389eb1a5d6bdf5900
JSY <soyoung6488@ajou.ac.kr>
2018-05-14 17:45:37 +0900

    2018-05-14-정소영-사용자 상세보기 커스텀 모달 title 동적으로 넣기 수정 및 버튼 넣음

--- Admin/public/template/js/custom.js
+++ Admin/public/template/js/custom.js
@@ -66,9 +66,11 @@
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         var modal = $(this);
-        $('#user_detailModalLabel').remove();
-        $('#user_detail_modal_header').append('<h4 class="modal-title" id="user_detailModalLabel">'+recipient+'</h4>');
-        modal.find('#user_detail_modal_body').text(recipient);
+        $('#user_detail_modal_header').empty();
+        $('#user_detail_modal_header').append('<h4 class="modal-title" id="user_detailModalLabel">'+recipient+'</h4>' +
+            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>');
+        $('#user_detail_modal_body').empty();
+        $('#user_detail_modal_body').append('<div class="col-lg-auto" id="user_detailModalContent">내용</div>');
     });
 
 })(jQuery);
