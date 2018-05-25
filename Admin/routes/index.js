80f6fc9eaa8babe0eb7cda52d5c6c9f99b68e85f
kobowon <ts3507@ajou.ac.kr>
2018-05-25 16:30:46 +0900

    2018-05-25-고보원-login페이지

--- Admin/routes/index.js
+++ Admin/routes/index.js
@@ -18,4 +18,7 @@ router.get('/page-contact' , function (req , res) {
     res.render('page-contact.html');
 })
 
+router.get('/login' , function (req , res) {
+    res.render('login.html');
+})
 module.exports = router;
