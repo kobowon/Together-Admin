39916e9b65f1af10616c5db47804dbb0322e5985
kobowon <ts3507@ajou.ac.kr>
2018-05-29 00:09:56 +0900

    2018-05-28-고보원-회원가입(helper)

--- Admin/routes/helper.js
+++ Admin/routes/helper.js
@@ -95,7 +95,7 @@ router.post('/device/save', function (req, res) {
                     }
                     else{//아이디 중복이 아니면
                         // var stmt = 'INSERT INTO user (userId,userPhone,userType,profileImage,deviceId) values(?,?,?,?,(select id from device where deviceKey = ?))';
-                        var statement = 'INSERT INTO user (userId,helperPwd,helperName,userPhone,userType,userFeedbackScore,profileImage,helpeeLatitude,helpeeLongitude,deviceId)' +
+                        var statement = 'INSERT INTO user (userId,helperPwd,helperName,userPhone,userType,userFeedbackScore,profileImage,latitude,longitude,deviceId)' +
                             'values(?,?,?,?,?,?,?,?,?,(select id from device where deviceKey = ?))';
                         var params = [user.userId,user.helperPwd,user.helperName,user.userPhone,user.userType,user.userFeedbackScore,user.profileImage,user.latitude,user.longitude,user.deviceKey];
                         connection.query(statement,params,function (err, result) {
