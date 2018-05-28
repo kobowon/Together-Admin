575bd7ba70eb8b447a75ff89efe6bddccea316ac
kobowon <ts3507@ajou.ac.kr>
2018-05-29 02:58:03 +0900

    2018-05-28-고보원-join

--- Admin/routes/api/api-join.js
+++ Admin/routes/api/api-join.js
@@ -38,40 +38,80 @@ router.get('/', function (request, response) {
     });
 });
 
-/*트렌젝션 예시
-* connection.beginTransaction(function(err) {
-  var post  = { m_tel: '010-1234-5678', m_id: 'GilDong2', m_pass: 'password1', m_name: '이순신',
-                m_date: '2000-01-01', m_gender: '남', m_email: 'GilDong@Hong.com' };
+router.post('/', function (request, response) {
+    //name -> user.userId
+    //phone -> user.userPhone
+    //title -> volunteeritem.content
+    //address -> volunteeritem.content
+    //startAt -> volunteeritem.time
+    //endAt ->  duration
+    //var time = (endAt - startAt) / (60 * 60 * 1000);
+    //var duration = Math.ceil(time);
+    //contents -> volunteeritem.content
+    //latitude -> user.latitude
+    //longitude -> user.longitude
 
-  if (err) { throw err; }
-  connection.query('INSERT INTO member SET ?', post, function(err, result) {
-    console.log(result);
-    if (err) {
-      connection.rollback(function() {
-        console.log("1.error");
-        throw err;
-      });
-    }
+    var queryStatement = '' +
+        'insert into request_join (name, phone, title, address , startAt , endAt , contents , latitude , longitude) ' +
+        'values (? , ? , ? , ? , ? , ? , ? , ? , ?)';
+    var requestBody = request.body;
+    var params = [requestBody.name, requestBody.phone, requestBody.title, requestBody.address, requestBody.startAt, requestBody.endAt, requestBody.contents, requestBody.latitude, requestBody.longitude];
 
-    //실행된 결과가 0일경우
-    connection.query('update member set m_gender = ? where m_name = ?', ['여','강감찬'], function(err, result) {
-      console.log(result);
-      if (!result.affectedRows) {
-        connection.rollback(function() {
-          console.log("2.error");
+    connectionPool.getConnection(function (err, connection) {
+        // Use the connection
+
+        connection.beginTransaction(function (err) {
+            if (err) {
+                throw err;
+            }
+            connection.query(queryStatement, params, function (err, result) {
+                // And done with the connection.
+                if (err) {
+                    console.error(err);
+                    connection.rollback(function () {
+                        console.error('rollback error');
+                        throw err;
+                    });
+                }//if err
+                console.log('insert transaction log');
+
+                queryStatement = 'insert into user (userId,name,userPhone,userType,latitude,longitude) values(?,?,?,?,?)'
+                pararms = [requestBody.phone, requestBody.name, requestBody.phone, 'institute', requestBody.latitude, requestBody.longitude];
+                connection.query(queryStatement, params, function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        connection.rollback(function () {
+                            console.error('rollback error');
+                            throw err;
+                        });
+                    }//if err
+                    //name -> user.userId
+                    //title -> volunteeritem.content
+                    //address -> volunteeritem.content
+                    //startAt -> volunteeritem.time
+                    //endAt ->  duration
+                    //var time = (endAt - startAt) / (60 * 60 * 1000);
+                    //var duration = Math.ceil(time);
+                    //contents -> volunteeritem.content
+                    queryStatement = 'insert into volunteeritem (helpeeId,userPhone,userType,latitude,longitude) values(?,?,?,?,?)'
+                    pararms = [requestBody.name, requestBody.phone, 'institute', requestBody.latitude, requestBody.longitude];
+                    connection.query(queryStatement, params, function (err, result) {
+                        if (err) {
+                            console.error(err);
+                            connection.rollback(function () {
+                                console.error('rollback error');
+                                throw err;
+                            });
+                        }//if err
+
+                    });
+
+                    connection.release();
+                    response.end();
+                });
+            })
         });
-      }
-      connection.commit(function(err) {
-        if (err) {
-          connection.rollback(function() {
-            throw err;
-          });
-        }
-        console.log('success!');
-      });
     });
-  });
 });
-* */
 
 module.exports = router;
