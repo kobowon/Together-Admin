886aaab998ed56e3730b7c59147362be0d3c9641
kobowon <ts3507@ajou.ac.kr>
2018-06-01 22:18:18 +0900

    2018-06-01-고보원-유저 &봉사 등록,신청,완료에 대한 시간간격별 봉사 개수 반환 api

--- Admin/repository/user/UserRepository.js
+++ Admin/repository/user/UserRepository.js
@@ -14,7 +14,7 @@ module.exports = function () {
         },
         //유저 id 넘기면 해당 type 가져오기
         selectType: function (userId, callback) {
-            var queryString = 'select userType from user where id = ?'
+            var queryString = 'select userType from user where userId = ?'
             query.executeWithData(queryString,userId,function (result) {
                 callback(result);
             });
@@ -22,31 +22,67 @@ module.exports = function () {
         //countWeeklyStandby
         //countWeeklyMatch
         //countWeeklyMatched
-        countHelpeeWeeklyStandby: function (userId,callback) {
+
+        /*****봉사 등록*******/
+        //1주일 간 특정 사용자가 요청한(봉사 등록)봉사 수(Helpee만 사용)
+        countWeeklyStandby: function (userId,callback) {
+            var queryString = 'select DATE_FORMAT(createdAt,\'%m-%d\') as w_date, count(volunteerId) as count ' +
+                'from volunteeritem where helpeeId = ? AND date(createdAt) > DATE_ADD(now(), INTERVAL-7 day) GROUP BY w_date';
+            query.executeWithData(queryString,userId,function (result) {
+                callback(result);
+            });
+        },
+        //(1,3,6)달 간 특정 사용자가 요청한(봉사 등록)봉사 수(Helpee만 사용)
+        countMonthlyStandby: function (userId,interval,callback) {
             var queryString = 'select DATE_FORMAT(createdAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
                 ' from volunteeritem' +
-                ' where helpeeId = ? AND date(createdAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
+                ' where helpeeId = ? AND date(createdAt) > DATE_ADD(now(), INTERVAL-'+interval+' month) GROUP BY w_date';
             query.executeWithData(queryString,userId,function (result) {
                 callback(result);
             });
         },
-        countHelpeeWeeklyMatch: function (userId,callback) {
+
+        /*****봉사 신청*******/
+        //1주일 간 특정 사용자가 신청한(봉사 신청)봉사 수(Helpee,Helper사용)
+        countWeeklyMatch: function (userId,callback) {
             var queryString = 'select DATE_FORMAT(matchAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
                 ' from volunteeritem' +
-                ' where helpeeId = ? AND date(matchAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
-            query.executeWithData(queryString,userId,function (result) {
+                ' where (helpeeId = ? OR helperId = ?) AND date(matchAt) > DATE_ADD(now(), INTERVAL-7 day) GROUP BY w_date';
+            var parms = [userId, userId];
+            query.executeWithData(queryString,parms,function (result) {
                 callback(result);
             });
         },
-        countHelpeeWeeklyMatched: function (userId,callback) {
+        //(1,3,6)달 간 특정 사용자가 신청한(봉사 신청)봉사 수(Helpee,Helper사용)
+        countMonthlyMatch: function (userId,interval,callback) {
+            var queryString = 'select DATE_FORMAT(matchAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
+                ' from volunteeritem' +
+                ' where (helpeeId = ? OR helperId = ?) AND date(matchAt) > DATE_ADD(now(), INTERVAL-'+interval+' month) GROUP BY w_date';
+            var parms = [userId, userId];
+            query.executeWithData(queryString,parms,function (result) {
+                callback(result);
+            });
+        },
+        /*****매칭 완료*******/
+        //1주일 간 특정 사용자가 수행한(매칭완료)봉사 수(Helpee,Helper사용)
+        countWeeklyMatched: function (userId,callback) {
             var queryString = 'select DATE_FORMAT(matchedAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
                 ' from volunteeritem' +
-                ' where helpeeId = ? AND date(matchedAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
-            query.executeWithData(queryString,userId,function (result) {
+                ' where  (helpeeId = ? OR helperId = ?) AND date(matchedAt) > DATE_ADD(now(), INTERVAL-7 day) GROUP BY w_date';
+            var parms = [userId, userId];
+            query.executeWithData(queryString,parms,function (result) {
+                callback(result);
+            });
+        },
+        //(1,3,6)달 간 특정 사용자가 수행한(매칭완료)봉사 수(Helpee,Helper사용)
+        countMonthlyMatched: function (userId,interval,callback) {
+            var queryString = 'select DATE_FORMAT(matchedAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
+                ' from volunteeritem' +
+                ' where (helpeeId = ? OR helperId = ?) AND date(matchedAt) > DATE_ADD(now(), INTERVAL-'+interval+' month) GROUP BY w_date';
+            var parms = [userId, userId];
+            query.executeWithData(queryString,parms,function (result) {
                 callback(result);
             });
         }
-
-
     }
 };
