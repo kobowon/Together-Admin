1ded2b8408895fee9f0240f0059996c237664bcf
kobowon <ts3507@ajou.ac.kr>
2018-06-01 18:49:03 +0900

    2018-06-01-고보원-user-userRepository api

--- Admin/repository/user/UserRepository.js
+++ Admin/repository/user/UserRepository.js
@@ -11,6 +11,42 @@ module.exports = function () {
             query.execute(queryString , function (result) {
                 callback(result);
             });
+        },
+        //유저 id 넘기면 해당 type 가져오기
+        selectType: function (userId, callback) {
+            var queryString = 'select userType from user where id = ?'
+            query.executeWithData(queryString,userId,function (result) {
+                callback(result);
+            });
+        },
+        //countWeeklyStandby
+        //countWeeklyMatch
+        //countWeeklyMatched
+        countHelpeeWeeklyStandby: function (userId,callback) {
+            var queryString = 'select DATE_FORMAT(createdAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
+                ' from volunteeritem' +
+                ' where helpeeId = ? AND date(createdAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
+            query.executeWithData(queryString,userId,function (result) {
+                callback(result);
+            });
+        },
+        countHelpeeWeeklyMatch: function (userId,callback) {
+            var queryString = 'select DATE_FORMAT(matchAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
+                ' from volunteeritem' +
+                ' where helpeeId = ? AND date(matchAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
+            query.executeWithData(queryString,userId,function (result) {
+                callback(result);
+            });
+        },
+        countHelpeeWeeklyMatched: function (userId,callback) {
+            var queryString = 'select DATE_FORMAT(matchedAt,\'%m-%d\') as w_date, count(volunteerId) as count' +
+                ' from volunteeritem' +
+                ' where helpeeId = ? AND date(matchedAt) > DATE_ADD(now(), INTERVAL-7day) GROUP BY w_date';
+            query.executeWithData(queryString,userId,function (result) {
+                callback(result);
+            });
         }
+
+
     }
 };
