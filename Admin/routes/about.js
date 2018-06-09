a849871e977efd156dc9b6ed6f9fe78209ba8d63
JSY <soyoung6488@ajou.ac.kr>
2018-06-09 19:09:48 +0900

    2018-06-09-정소영-intro수정하고 about-us 페이지로 옮김

---
+++ Admin/routes/about.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var mysql_dbc = require('../db/db_con')();
+var connectionPool = mysql_dbc.createPool();
+var accessRepository = require('../repository/access/AccessRepository')();
+var query = require('../db/db_wrap')();
+
+router.get('/', function (request, response) {
+
+    accessRepository.save('/about-us' , function () {
+        var queryStatement =
+            'SELECT v.helpeeFeedbackContent, u.profileImage ' +
+            'FROM volunteeritem AS v ' +
+            'JOIN user AS u ON v.helpeeId = u.userId ' +
+            'AND helpeeFeedbackContent IS NOT NULL order by date desc limit 5';
+
+        query.execute(queryStatement , function (result) {
+            response.render('about-us/index.ejs' , {feedbackList : result , moment : moment});
+        });
+    });
+
+});
+
+module.exports = router;
