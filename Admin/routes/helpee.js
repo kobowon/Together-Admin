a2700c133b5c7fa553e7ef42deb18c2e84a541dd
kobowon <ts3507@ajou.ac.kr>
2018-06-11 19:43:29 +0900

    2018-06-11-고보원-matchAt

--- Admin/routes/helpee.js
+++ Admin/routes/helpee.js
@@ -229,7 +229,8 @@ router.put('/name-age', function (req, res) {
             time: body.time,//18:30:00
             duration: body.duration,//3
             matchingStatus: 0,
-            startStatus: 0
+            startStatus: 0,
+            matchAt : now()
         };
         connectionPool.getConnection(function (err, connection) {
             // Use the connection
@@ -364,8 +365,6 @@ router.put('/volunteer/complete', function (req, res) {
     });
 });
 
-
-
 //helperName 이름 가져오기
 router.get('/helper/name/:userId', function (req, res) {
     connectionPool.getConnection(function (err, connection) {
@@ -418,7 +417,7 @@ router.put('/token/update', function (req, res) {
 
 //봉사 시작(volunterId -> startStatus = 1 & helper 한테 푸시)
 router.put('/volunteer/start', function (req, res) {
-    var stmt = 'UPDATE volunteeritem SET startStatus = ? , helperDepartStatus = "arrive" WHERE volunteerId = ?';
+    var stmt = 'UPDATE volunteeritem SET startStatus = ? , helperDepartStatus = "arrive", startAt = now() WHERE volunteerId = ?';
     var params = [1,req.body.volunteerId];
     connectionPool.getConnection(function (err, connection) {
         // Use the connection
