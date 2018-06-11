b3a83834b9a7169e129fdded569984a709d1f96c
kobowon <ts3507@ajou.ac.kr>
2018-06-11 20:30:49 +0900

    2018-06-11-고보원-periodically

--- Admin/repository/volunteer/VolunteerItemRepository.js
+++ Admin/repository/volunteer/VolunteerItemRepository.js
@@ -48,7 +48,7 @@ module.exports = function () {
             var queryString =
                 'select helpeeId, helperId, date_add(NOW(),INTERVAL 1 HOUR) AS FUTURE,volunteerId,time ' +
                 'from volunteeritem ' +
-                'where time(now())<volunteeritem.time AND volunteeritem.time < date_add(NOW(),INTERVAL 1 HOUR) AND (date(now())=volunteeritem.date)';
+                'where now()<volunteeritem.time AND volunteeritem.time < date_add(NOW(),INTERVAL 1 HOUR) AND (date(now())=volunteeritem.date)';
             query.execute(queryString, function (result) {
                     callback(result);
                 })
