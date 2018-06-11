a7d81d482c40a8170c40b413050913662c770bc6
JSY <soyoung6488@ajou.ac.kr>
2018-06-12 03:05:54 +0900

    2018-06-12-정소영-봉사정렬 수정

--- Admin/public/template/js/approve.js
+++ Admin/public/template/js/approve.js
@@ -26,7 +26,7 @@
             },
             success: function (volData) {
                 var i=0,length=Object.keys(volData).length;
-                sort_down_by_date(volData);
+                //sort_down_by_date(volData);
                 for(i; i<=length-1;i++)
                 {
                     var $vol_list_div= $(
@@ -37,7 +37,7 @@
                                 '</div>' +
                             '</div>' +
                         '</div>');
-                    $('.main').append($vol_list_div);
+                    $('.main').prepend($vol_list_div);
 
 /*                    var volunteerType;
                     var icon;
